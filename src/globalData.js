14188bb73ff69548b04e956be95d07b5291899f5
qb-oss <64483965+qb-oss@users.noreply.github.com>
2022-10-01 13:46:31 +0200

    Update globalData.js

--- src/globalData.js
+++ src/globalData.js
@@ -1,6 +1,6 @@
 let background = backgrounds.VILLAGE;
 let gif = gifs.CLOUDS;
 let promptCursor = promptCursors[1];
-let quote = "Deutschland, Deutschland über allen";
+let quote = "七転び八起き";
 let greetingsText = "Welcome home, human! \\^o^/";
 let bookmarks = [];
